ed61fa182607000d3c039871ce7285ae992eb20f
sean2077 <sean2077@163.com>
2025-07-26 18:18:55 +0800

    feat: 添加两个货物问题的动态规划实现

---
+++ algo/dp/knapsack-dp/twoCargo.cpp
@@ -0,0 +1,73 @@
+/*
+某公司有一批货物，系了 2 个轮船进行运输。每条轮船上可以运输不同容量的货物。由于 2 个轮船的发船时间不一样，同一个货物通过不同的轮船运输到终点后，利润也是不一样的，现在需要根据不同轮船的容量、货物的容量以及在不同轮船上进行运输的利润有效地分配货物，从而达到货物价值最大化。每个货物只有 1 件。
+输入：
+第一行输入为 A B M，A 代表轮船 A 的容量，B 代表轮船 B 的容量，M 代表货物的个数。
+之后 M 行分别代表每个货物的容量、使用轮船 A 运输的利润、使用轮船 B 运输的利润。
+输出：
+最大利润。
+例子：
+输入：
+5 6 4
+4 3 2
+3 1 4
+2 2 4
+2 3 3
+输出：11
+解释：轮船 A 运输货物 1 或者 4，利润 3；轮船 B 运输货物 2 和 3，利润 8；所以最大的利润是 11。
+*/
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+#include "print_utils.hpp"
+
+using namespace std;
+
+/** 两个货物. TC: O(ABN) SC: O(AB) */
+int twoCargo_dp(int A, int B, const vector<int>& capacities, const vector<int>& profitsA, const vector<int>& profitsB) {
+    int N = capacities.size();
+    vector<vector<int>> dp(A + 1, vector<int>(B + 1, 0)); // A 装 i，B 装 j 时的最大利润
+    for (int i = 0; i < N; ++i) {
+        for (int a = A; a > 0; --a) {
+            for (int b = B; b > 0; --b) {
+                if (a >= capacities[i] && b >= capacities[i]) { // 选A、B或不选
+                    dp[a][b] = max(dp[a][b], max(dp[a - capacities[i]][b] + profitsA[i], dp[a][b - capacities[i]] + profitsB[i]));
+                } else if (a >= capacities[i]) { // 选A或不选
+                    dp[a][b] = max(dp[a][b], dp[a - capacities[i]][b] + profitsA[i]);
+                } else if (b >= capacities[i]) { // 选B或不选
+                    dp[a][b] = max(dp[a][b], dp[a][b - capacities[i]] + profitsB[i]);
+                }
+            }
+        }
+    }
+    return dp[A][B];
+}
+
+int main() {
+    cout << "Two Cargo Problem" << endl;
+
+    // 测试用例：
+    int A = 5, B = 6, M = 4;
+    vector<int> capacities = {4, 3, 2, 2};
+    vector<int> profitsA = {3, 1, 2, 3};
+    vector<int> profitsB = {2, 4, 4, 3};
+
+    cout << "A: " << A << ", B: " << B << ", M: " << M << endl;
+    cout << "Capacities: " << capacities << endl;
+    cout << "Profits A: " << profitsA << endl;
+    cout << "Profits B: " << profitsB << endl;
+
+    auto test = [&](const string& method_name,
+                    int (*method)(int, int, const vector<int>&, const vector<int>&, const vector<int>&)) {
+        auto start = chrono::high_resolution_clock::now();
+        int result = method(A, B, capacities, profitsA, profitsB);
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+        cout << method_name << " Result: " << result << ", Time: " << duration << " microseconds" << endl;
+    };
+
+    test("twoCargo_dp", twoCargo_dp);
+
+    return 0;
+}
