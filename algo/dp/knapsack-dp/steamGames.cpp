68524edf428439fbf356af9b1b53eafc072b581b
sean2077 <sean2077@163.com>
2025-07-26 17:52:00 +0800

    feat: 添加 streamGames 分组背包算法实现

--- algo/dp/knapsack-dp/steamGames.cpp
+++ algo/dp/knapsack-dp/steamGames.cpp
@@ -44,9 +44,7 @@ int steamGames_dp(const vector<int>& prices, const vector<int>& values, const ve
         sort(dlc_ids.begin(), dlc_ids.end(), [&](int l, int r) { return prices[l] < prices[r]; });
 
         for (int j = M; j >= prices[i]; --j) {
-            // 买游戏主体
-            dp[j] = max(dp[j], dp[j - prices[i]] + values[i]);
-            // 再枚举DLC的所有选购组合，可以用二进制枚举，也可以dfs递归枚举
+            // 枚举DLC的所有选购组合，可以用二进制枚举，也可以dfs递归枚举
             // 二进制枚举直观简单，但性能可能不如递归
             for (uint32_t mask = 0; mask < (1u << dlc_cnt); ++mask) {
                 int sum_price = prices[i], sum_value = values[i]; // 捆绑购买主体
@@ -67,6 +65,50 @@ int steamGames_dp(const vector<int>& prices, const vector<int>& values, const ve
     return dp[M];
 }
 
+// 分组背包合并 SC: O(MN+n*M^2)
+int steamGames_grouped_dp(const vector<int>& prices, const vector<int>& values, const vector<int>& indices, int M) {
+    int N = prices.size();
+    unordered_map<int, vector<int>> dlc_id_map;
+    for (int i = 0; i < N; ++i) {
+        if (indices[i] != -1) {
+            dlc_id_map[indices[i]].emplace_back(i);
+        }
+    }
+    vector<int> dp(M + 1, 0);
+    for (int i = 0; i < N; ++i) {
+        if (indices[i] != -1) {
+            continue; // 只处理主件
+        }
+        // 组内0-1背包：主件必须买，DLC可选
+        vector<pair<int, int>> group_items; // (price, value)
+        group_items.emplace_back(prices[i], values[i]);
+        for (int k : dlc_id_map[i]) {
+            group_items.emplace_back(prices[k], values[k]);
+        }
+        int group_size = group_items.size();
+        // 组内dp，f[x]=主件+部分DLC总价为x时的最大耐玩值
+        vector<int> f(M + 1, -1);
+        f[group_items[0].first] = group_items[0].second; // 必须买主件
+        for (int j = 1; j < group_size; ++j) {
+            int p = group_items[j].first, v = group_items[j].second;
+            for (int b = M; b >= p; --b) {
+                if (f[b - p] != -1) {
+                    f[b] = max(f[b], f[b - p] + v);
+                }
+            }
+        }
+        // 分组背包合并
+        for (int j = M; j >= prices[i]; --j) {
+            for (int k = prices[i]; k <= j; ++k) {
+                if (f[k] != -1) {
+                    dp[j] = max(dp[j], dp[j - k] + f[k]);
+                }
+            }
+        }
+    }
+    return dp[M];
+}
+
 int main() {
     cout << "Steam Games Problem" << endl;
 
@@ -75,6 +117,7 @@ int main() {
     vector<int> prices = {100, 23, 59, 69, 30, 40};
     vector<int> values = {4, 3, 5, 2, 6, 7};
     vector<int> indices = {-1, 0, 0, -1, 3, -1};
+    // result: 16
 
     cout << "M: " << M << endl;
     cout << "Prices: " << prices << endl;
@@ -92,6 +135,7 @@ int main() {
     };
 
     test("steamGames_dp", steamGames_dp);
+    test("steamGames_grouped_dp", steamGames_grouped_dp);
 
     return 0;
 }
